83f77890d89ab1eb16bda1824bdb1138a0626604
JT Klenke <jtk96@cornell.edu>
2023-07-23 19:13:28 -0400

    Phone drop-down menu done
    - Finished the hamburger drop-down menu, very simple inplementation
    - Added parallax scrolling to the home screen
    - Fixed some margin issues

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const root = document.querySelector(':root');
+var hamburgerIcon = null
 
 document.addEventListener("DOMContentLoaded", function () {
     const smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
 
+
     smoothScrollLinks.forEach(function (link) {
         link.addEventListener("click", function (event) {
             event.preventDefault();
+            root.style.setProperty("--show-dropdown", "hidden")
+            hamburgerIcon.src = "images/hamburger.svg";
+
             const target = this.getAttribute("href");
             const samePage = target.startsWith("#");
             console.log(samePage)
@@ -19,4 +25,23 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+    const hamburgerButton = document.querySelector('.hamburger');
+    hamburgerIcon = hamburgerButton.querySelector('img');
+
 });
+
+
+function handleDropdown() {
+
+    if (getComputedStyle(root).getPropertyValue('--show-dropdown') == "hidden") {
+        root.style.setProperty("--show-dropdown", "visible")
+        hamburgerIcon.src = "images/x.svg";
+
+    } else {
+        root.style.setProperty("--show-dropdown", "hidden")
+        hamburgerIcon.src = "images/hamburger.svg";
+
+    }
+
+
+}
